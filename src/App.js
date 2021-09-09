53b76334aa7f6ceef488052d4ff39f4409c2f95a
Jacob Johnson <johnsondljl7@yahoo.com>
2021-09-09 11:44:46 -0500

    first draft, fully functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import PokeNames from "./components/pokemonNames";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+            .then((response) => response.json())
+            .then((response) => setPokemon(response.results));
+    });
+
+    return (
+        <div className="App">
+            <PokeNames allPokemon={pokemon} />
+        </div>
+    );
 }
 
 export default App;
